a0c65996d58963ea5b365ba0ba053e2be94e5d43
Shubham <shubhamsinha2050@gmail.com>
2019-11-04 22:07:41 +0530

    sending jsonResponse

--- pure_django_api/urls.py
+++ pure_django_api/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from . import settings
 from django.conf.urls.static import static
+from updates.views import update_model_detail_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', update_model_detail_view)
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- updates/views.py
+++ updates/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+
+# def detail_view(request):
+#     return render()
+
+from .models import Update
 
 # Create your views here.
+
+
+def update_model_detail_view(request):
+    # URI -- for a REST Api
+
+    data = {
+        "count": 1000,
+        "content": "Some new Content"
+    }
+    return JsonResponse(data)
